84dabdb36950cbb34f1546cd3fe4060b219624d8
mauvedeity <me@mauvedeity.com>
2024-10-09 17:18:31 +0100

    Making a bit of progress

--- exporter.py
+++ exporter.py
@@ -4,7 +4,10 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('blog-dump.xml')
 root = tree.getroot()
 
-for child in root:
-  print(child.tag, child.attrib)
+posts = root.findall('{http://www.w3.org/2005/Atom}entry')
 
+p = posts[0]
+print (list(p))
 
+q=p[0]
+print(q.text)
