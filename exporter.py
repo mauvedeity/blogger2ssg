6a7f7961ed069ec84c5ea358776cd5a4ddd4dbef
mauvedeity <me@mauvedeity.com>
2024-11-13 11:34:52 +0000

    Lots of progress, now exports posts correctly but with IDs not names

--- exporter.py
+++ exporter.py
@@ -1,22 +1,54 @@
 #!/usr/bin/env python3
 #
 import xml.etree.ElementTree as ET
-tree = ET.parse('blog-dump.xml')
-root = tree.getroot()
+from datetime import datetime
+import os
+import time
+#
+#
+def getPostID(pstr):
+  return(pstr.split('-')[2])
 
-posts = root.findall('{http://www.w3.org/2005/Atom}entry')
+def titleUnmung(pstr):
+  sl = str(pstr).splitlines()
+  if(len(sl)== 1):
+    return(sl[0])
+  else:
+    rv = ''
+    for i in sl:
+      rv += i.strip() + ' '
+    return(rv.strip())
 
-# print("p")
-# p = posts[0]
-# print (list(p))
-# 
-# print("q")
-# q=p[0]
-# print(q.text)
+def processEntry(entry):
+  etitle = entry.find('{http://www.w3.org/2005/Atom}title').text
+  econtent = entry.find('{http://www.w3.org/2005/Atom}content').text
+  epubdate = entry.find('{http://www.w3.org/2005/Atom}published').text
+  epubid = entry.find('{http://www.w3.org/2005/Atom}id').text
+  if (etitle == None): # one post doesn't have a title
+    etitle = epubid
+  print(titleUnmung(etitle), getPostID(epubid),sep='|')
+  fname = getPostID(epubid) + '.html'
+  with open(fname, 'wt') as outf:
+    outf.write('<h1>' + etitle + '</h1>\n')
+    outf.write('\n')
+    outf.write(econtent)
+    outf.write('\n')
+  tm = datetime.strptime(epubdate,'%Y-%m-%dT%H:%M:%S.%f%z')
+  os.utime(fname,(tm.timestamp(),tm.timestamp()))
 
-for p in posts:
-  q = p[0]
-  qt = q.text
-  if(qt.find("post-")):
-    print(qt)
-    exit()
+def main():
+  tree = ET.parse('blog-dump.xml')
+  root = tree.getroot()
+  entries = root.findall('{http://www.w3.org/2005/Atom}entry')
+
+  ekount = 0
+  for e in entries:
+    e_id = e.find('{http://www.w3.org/2005/Atom}id').text
+    if 'post-' in e_id:
+      processEntry(e)
+      ekount += 1
+      if(ekount > 10):
+        exit()
+
+if __name__ == '__main__':
+  main()
