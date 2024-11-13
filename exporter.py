95b160cddc1242d39d9a17b691b970429b1b751a
mauvedeity <me@mauvedeity.com>
2024-11-13 11:32:59 +0000

    Last change

--- exporter.py
+++ exporter.py
@@ -6,8 +6,17 @@ root = tree.getroot()
 
 posts = root.findall('{http://www.w3.org/2005/Atom}entry')
 
-p = posts[0]
-print (list(p))
+# print("p")
+# p = posts[0]
+# print (list(p))
+# 
+# print("q")
+# q=p[0]
+# print(q.text)
 
-q=p[0]
-print(q.text)
+for p in posts:
+  q = p[0]
+  qt = q.text
+  if(qt.find("post-")):
+    print(qt)
+    exit()
