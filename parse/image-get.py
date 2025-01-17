8d5b55b17c8acfba2bea073fa0bca8b1b9d85427
mauvedeity <me@mauvedeity.com>
2025-01-17 16:57:23 +0000

    Start of basic image ripper code

---
+++ parse/image-get.py
@@ -0,0 +1,49 @@
+#!env python3
+
+import urllib.request
+import shutil
+from os.path import basename, exists
+from html.parser import HTMLParser
+
+# Global URL header
+#
+glHeader = ''
+
+# HTML Parser
+#
+class myHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if(tag == 'img'):
+            for attr in attrs:
+                if attr[0] == 'src':
+                    srcname = attr[1]
+            if exists('dest/'+srcname):     ## hardwired, plz fix
+                print('Skipped', srcname)
+            else:
+                print('Fetching',srcname)
+                url2file(srcname, 'dest')   ## hardwired, plz fix
+    def handle_endtag(self, tag):
+        pass
+    def handle_data(self, data):
+        pass
+
+# Image Grabber
+#
+def url2file(sURL, sDestPath):
+    sDest = sDestPath + '/' + basename(sURL)
+    sURL = glHeader + sURL
+    with urllib.request.urlopen(sURL) as response, open(sDest,'wb') as outfile:
+        data = response.read()
+        outfile.write(data)
+
+def getAllImages(sURL):
+    sURL = glHeader + sURL
+    with urllib.request.urlopen(sURL) as response:
+        data = response.read()
+        print(data)
+        indexdata = data.decode("utf-8")
+        parser = myHTMLParser()
+        parser.feed(indexdata)
+
+glHeader = 'http://localhost:8091/'
+getAllImages('index.html')
