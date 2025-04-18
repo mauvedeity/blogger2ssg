360142bc4f2f8e43d872d56ff3020b6881aa7922
mauvedeity <rhys.wilkins@gmail.com>
2025-04-18 11:56:16 +0100

    Fixed the parse stuff

--- parse/example-parse.py
+++ parse/example-parse.py
@@ -1,10 +1,21 @@
 #!env python3
 
+strURLFrom = 'https://uselessofblog.blogspot.com'
+strURLTo = '../..q'
+
 from html.parser import HTMLParser
 
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag, " with attrs ", attrs)
+        print("Encountered a start tag:", tag, "with attrs ", attrs)
+        # if the tag is an A, then look at the atts and parse
+        if (tag == 'a'):
+            attrlist = attrs
+            print('a', attrlist[0])
+            hr, data = attrlist[0]
+            print(data)
+            data = data.replace(strURLFrom, strURLTo)
+            print(data)
 
     def handle_endtag(self, tag):
         print("Encountered an end tag :", tag)
@@ -14,8 +25,8 @@ class MyHTMLParser(HTMLParser):
 
 parser = MyHTMLParser()
 
-with open('another-hurdle-for-cryptocurrencies.html','r') as htmlsrc:
+with open('it-was-all-going-so-well.html','r') as htmlsrc:
     htmlcontent = htmlsrc.read()
 
-print(htmlcontent)
+# print(htmlcontent)
 parser.feed(htmlcontent)
